432fd2c7699ad10275af3b06b85a16b80cb0dc6f
Zohaib Butt <you@example.com>
2020-11-14 19:10:50 +0500

    a small change

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,12 @@ import Fib from "./Fib";
 
 class App extends Component {
   render() {
-    console.log("New-1");
     return (
       <Router>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calculator</h1>
             <Link to="/">Home</Link>
             <span> </span>
             <a href="https://www.google.com">Other Page</a>
