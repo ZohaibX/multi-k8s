dd9e07b4e804d5226c6685bce6374318bbc605a4
Zohaib Butt <you@example.com>
2020-11-14 18:46:05 +0500

    Deploy To AWS config added

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <span> </span>
+            <a href="https://www.google.com">Other Page</a>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
