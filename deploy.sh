57fd7475a439ae96b2f7d31a4f7a129a5d0d46dd
Zohaib Butt <you@example.com>
2020-12-09 22:03:33 +0500

    New Changes Made ...

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,20 @@
 docker build -t 03004968719/multi-client:latest -t 03004968719/multi-client:$SHA -f ./client/Dockerfile ./client 
 docker build -t 03004968719/multi-server:latest -t 03004968719/multi-server:$SHA -f ./server/Dockerfile ./server 
 docker build -t 03004968719/multi-worker:latest -t 03004968719/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
+
 docker push 03004968719/multi-client:latest
 docker push 03004968719/multi-server:latest
 docker push 03004968719/multi-worker:latest
+
 docker push 03004968719/multi-client:$SHA
 docker push 03004968719/multi-server:$SHA
 docker push 03004968719/multi-worker:$SHA
+
 kubectl apply -f k8s 
+
 kubectl set image deployments/server-deployment server=03004968719/multi-server:$SHA 
 kubectl set image deployments/client-deployment client=03004968719/multi-client:$SHA 
 kubectl set image deployments/worker-deployment worker=03004968719/multi-worker:$SHA 
 # server-deployment is the name of server-deployment file , server=03004968719/multi-server means that in container server -- we need to run our own image with tag $SHA
+# building docker images above ...
+# applying kubernetes files in kubectl command
